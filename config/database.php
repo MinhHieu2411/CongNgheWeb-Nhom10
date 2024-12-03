6c68b411337cb42c7a405ccb52192a87a14bbb42
Nguyen Anh Thi <anhthi28042004@gmail.com>
2024-12-03 19:22:53 +0700

    Your commit message

--- config/database.php
+++ config/database.php
@@ -1,1 +1,22 @@
 <?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "tintuc"; // Tên cơ sở dữ liệu
+    private $username = "root";  // Tài khoản
+    private $password = "";      // Mật khẩu
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
