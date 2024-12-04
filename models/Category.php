5042b6bd61ae2d23aec6a396c1746091eb27b496
Nguyen Anh Thi <anhthi28042004@gmail.com>
2024-12-04 20:01:38 +0700

    Remove unnecessary files in hieu's-works branch

--- models/Category.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Category {
-    public function getAll() {
-        $db = new PDO('mysql:host=localhost;dbname=tintuc', 'root', '');
-        $stmt = $db->query('SELECT * FROM categories');
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-}
